bcc384a8f374bc291a977b84fd4219b963850f4c
shivangi2000-dev <shivangirai417@gmail.com>
2024-12-04 12:10:50 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 DATABASE_CONFIG = {
-    "dbname": "cse560",
+    "dbname": "postgres",
     "user": "postgres",
     "password": "Buffalo123456",  # Your actual password
     "host": "cse560.c1e4q8q46yi1.us-east-2.rds.amazonaws.com",  # Host endpoint
